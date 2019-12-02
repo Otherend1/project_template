6757a860af769a080dd5379e51e0d78418bdac3b
Otherend1 <remisemichel@gmail.com>
2019-12-02 11:16:56 +0100

    ADD: git LFS (large file storage) example

--- python/iris/settings.py
+++ python/iris/settings.py
@@ -1,5 +1,8 @@
+import os
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+
 settings = {
    "dumps": {
-       "iris_svm_model_path" : "/extra_data/dumps/iris_svm_model.joblib",
+       "iris_svm_model_path" : os.path.join(cur_dir, "model/weights/iris_svm_model.joblib"),
    }
 }
